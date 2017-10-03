8b103ee7e788caebbe396ace78325934b2d0dbb7
SMUSFTC <SMUSFTCTeam1>
2017-10-02 17:37:28 -0700

    Started work on the GamepadEventManager class, which will be managing all the events that are recieved from the connected remote controls at game-time.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadEventManager.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+/**
+ * Created by alex.fanat on 9/26/2017.
+ */
+// NOTE: See this useful link for more information: https://stackoverflow.com/questions/13604703/how-do-i-define-a-method-which-takes-a-lambda-as-a-parameter-in-java-8.
+
+    /*
+    Class Diagram:
+
+        RobotController(targetGamepad)
+        void hook()
+
+        static ArrayList<Gamepad> getAvailableGamepads()
+
+        Gamepad targetGamepad
+        BiConsumer<Boolean, Boolean> handleDPadUpActivated, handleDPadDownActivated, handleDPadLeftActivated, handleDPadRightActivated, JoystickLeftActivated, JoystickRightActivated, BumperLeftActivated, BumperRightActivated
+        Consumer<Integer, Integer> JoystickLeftXChanged, JoyStickLeftYChanged, JoystickRightXChanged, JoystickRightYChanged, TriggerLeftPressureChanged
+
+    */
+
+public class GamepadEventManager
+{
+    public interface GamepadEventHandler<T> { void handleGamepadEvent(T currentValue, T previousValue); }
+    public enum HandlerTriggerMode { TICK, CHANGE }
+
+    public GamepadEventManager(Gamepad targetGamepad) { }
+    public void runEventLoop(HandlerTriggerMode triggerMode) { }
+
+    GamepadEventHandler<Boolean> dPadUpHandler, dPadDownHandler, dPadLeftHandler, dPadRightHandler, leftJoystickZHandler, rightJoystickZHandler;
+    GamepadEventHandler<Integer> leftJoystickXHandler, leftJoystickYHandler, rightJoystickXHandler, rightJoystickYHandler;
+}
