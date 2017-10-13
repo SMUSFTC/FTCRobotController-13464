9087a3424045b84403a7d99556a05898fdbcf301
SMUSFTC <SMUSFTCTeam1>
2017-10-12 18:43:31 -0700

    More work done on GamepadValueMonitor. Core functionality still needs to be implemented.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadValueMonitor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadValueMonitor.java
@@ -1,10 +1,9 @@
 package org.firstinspires.ftc.teamcode;
+
 import com.qualcomm.robotcore.hardware.Gamepad;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CyclicBarrier;
 
 /**
  * Created by alex.fanat on 9/26/2017.
@@ -13,16 +12,33 @@ import java.util.concurrent.CyclicBarrier;
 
 public class GamepadValueMonitor
 {
-    public interface ChangeInformer { void inform(); }
-    public interface ValueFetcher<T> { T fetch(); }
-    public static class MonitoredValue<T> { public void updateValue() { previousValue = currentValue; currentValue = fetcher.fetch(); if (tickBased || previousValue != currentValue) informer.inform(); } public boolean tickBased = true; public ValueFetcher<T> fetcher; public ChangeInformer informer; T currentValue, previousValue; }
+    public interface Fetchable<T> { T fetch(); }
+    public static class MonitoredValue<T> { public enum UpdateMode { TICK, CHANGE } public void updateValue() { previousValue = currentValue; currentValue = value.fetch(); if (tickBased || previousValue != currentValue) updateInformer.run(); } public boolean tickBased = true; public Fetchable<T> value; public Runnable updateInformer; T currentValue, previousValue; UpdateMode activeUpdateMode = UpdateMode.TICK; }
+    public static class MonitoredGamepadValue<T> extends MonitoredValue<T> { public boolean monitorValue = false; }
 
-    MonitoredValue<Boolean> leftStickButton, rightStickButton, leftBumper, rightBumper, a, b, x, y, start, back, dPadUp, dPadDown, dPadLeft, dPadRight;
-    MonitoredValue<Float> leftStickX, leftStickY, rightStickX, rightStickY, leftTrigger;
+    final MonitoredGamepadValue<Boolean>
+            leftStickButton = new MonitoredGamepadValue<Boolean>() {{ value = new Fetchable() { @Override public Boolean fetch() { return targetGamepad.left_stick_button; } }; }},
+            rightStickButton = new MonitoredGamepadValue<Boolean>() {{ value = new Fetchable() { @Override public Boolean fetch() { return targetGamepad.right_stick_button; } }; }},
+            leftBumper = new MonitoredGamepadValue<Boolean>() {{ value = new Fetchable() { @Override public Boolean fetch() { return targetGamepad.left_bumper; } }; }},
+            rightBumper = new MonitoredGamepadValue<Boolean>() {{ value = new Fetchable() { @Override public Boolean fetch() { return targetGamepad.right_bumper; } }; }},
+            a = new MonitoredGamepadValue<Boolean>() {{ value = new Fetchable() { @Override public Boolean fetch() { return targetGamepad.a; } }; }},
+            b = new MonitoredGamepadValue<Boolean>() {{ value = new Fetchable() { @Override public Boolean fetch() { return targetGamepad.b; } }; }},
+            x = new MonitoredGamepadValue<Boolean>() {{ value = new Fetchable() { @Override public Boolean fetch() { return targetGamepad.x; } }; }},
+            y = new MonitoredGamepadValue<Boolean>() {{ value = new Fetchable() { @Override public Boolean fetch() { return targetGamepad.y; } }; }},
+            start = new MonitoredGamepadValue<Boolean>() {{ value = new Fetchable() { @Override public Boolean fetch() { return targetGamepad.start; } }; }},
+            back = new MonitoredGamepadValue<Boolean>() {{ value = new Fetchable() { @Override public Boolean fetch() { return targetGamepad.back; } }; }},
+            dPadUp = new MonitoredGamepadValue<Boolean>() {{ value = new Fetchable() { @Override public Boolean fetch() { return targetGamepad.dpad_up; } }; }},
+            dPadDown = new MonitoredGamepadValue<Boolean>() {{ value = new Fetchable() { @Override public Boolean fetch() { return targetGamepad.dpad_down; } }; }},
+            dPadLeft = new MonitoredGamepadValue<Boolean>() {{ value = new Fetchable() { @Override public Boolean fetch() { return targetGamepad.dpad_left; } }; }},
+            dPadRight = new MonitoredGamepadValue<Boolean>() {{ value = new Fetchable() { @Override public Boolean fetch() { return targetGamepad.dpad_right; } }; }};
+    final MonitoredGamepadValue<Float> leftStickX = new MonitoredGamepadValue(), leftStickY = new MonitoredGamepadValue(), rightStickX = new MonitoredGamepadValue(), rightStickY = new MonitoredGamepadValue(), leftTrigger = new MonitoredGamepadValue();
 
-    public List<MonitoredValue> monitoredValues = new ArrayList<MonitoredValue>();
+    List<MonitoredValue> monitoredValues = new ArrayList<MonitoredValue>();
+    Gamepad targetGamepad;
 
-    public GamepadValueMonitor(List<MonitoredValue> valuesToMonitor) { monitoredValues = new ArrayList<>(valuesToMonitor); }
+    public GamepadValueMonitor(Gamepad targetGamepad) {  }
+
+    public GamepadValueMonitor(GamepadValueMonitor other) { ; } // Needs implementation.
 
     public void initialize()
     {
