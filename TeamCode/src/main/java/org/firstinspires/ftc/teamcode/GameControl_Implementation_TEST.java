f9a194ef0fd3b009d560878f9151f610117f2136
SMUSFTC <SMUSFTCTeam1>
2017-10-14 15:19:01 -0700

    Started work on GameControl_Implementation_TEST.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GameControl_Implementation_TEST.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GameControl_Implementation_TEST.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by alex.fanat on 12/3/2016.
@@ -12,33 +13,22 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 @TeleOp(name="Single Controller",group="Linear OpMode")
 public class GameControl_Implementation_TEST extends LinearOpMode
 {
-    DcMotor left;
-    DcMotor right;
+    Servo armServoLeft;
+    Servo armServoRight;
     @Override public void runOpMode() throws InterruptedException
     {
-        boolean wheelsInverse=false;
+        armServoLeft = hardwareMap.servo.get("armServoLeft");
+        armServoRight = hardwareMap.servo.get("armServoRight");
 
-        right=hardwareMap.dcMotor.get("right");
-        left=hardwareMap.dcMotor.get("left");
-        left.setDirection(DcMotorSimple.Direction.REVERSE);
+        armServoLeft.setPosition(0.5144);
+        armServoRight.setPosition(0.39);
 
-        GamepadEventManager.Configuration eventManagerConfiguration = new GamepadEventManager.Configuration();
-        eventManagerConfiguration.dPadDownPressionHandler = new GamepadEventManager.GamepadEventHandler<Boolean>() {
-            @Override public void handleGamepadEvent(Boolean currentValue, Boolean previousValue)
-            {
-
-            }
-        };
-
-        //GamepadEventManager gamepadEventManager = new GamepadEventManager(gamepad1, eventManagerConfiguration, GamepadEventManager.GamepadEventHandler.TriggerMode.CHANGE);
-
+        GamepadValueMonitor valueMonitor = new GamepadValueMonitor(gamepad1, new GamepadValueMonitor.Fetchable<Boolean>() { @Override public Boolean fetch() { return opModeIsActive(); } })
+        {{
+            dPadUp.updateInformer = new Runnable() { @Override public void run() { /* Code goes here. */ } };
+            dPadUp.active = true;
+        }};
         waitForStart();
-
-        while(opModeIsActive())
-        {
-            if (gamepad1.b) wheelsInverse=!wheelsInverse;
-            left.setPower((wheelsInverse?-1:1)*(gamepad1.left_stick_y)+gamepad1.left_stick_x);
-            right.setPower((wheelsInverse?-1:1)*(gamepad1.left_stick_y)-gamepad1.left_stick_x);
-        }
+        valueMonitor.startMonitoring(true);
     }
 }
