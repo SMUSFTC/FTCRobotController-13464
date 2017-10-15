54a3e17b170d4f5abcbc3d7a54966c1f043f6e84
Alex Fanat <alex.fanat@smus.ca>
2017-10-14 21:22:44 -0700

    Created Servo_Control_Test class, which makes use of GamepadValueMonitor to initiate basic movements of the arm/claw system servos.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Servo_Control_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Servo_Control_Test.java
@@ -10,25 +10,32 @@ import com.qualcomm.robotcore.hardware.Servo;
  * Created by alex.fanat on 12/3/2016.
  */
 
-@TeleOp(name="Single Controller",group="Linear OpMode")
-public class GameControl_Implementation_TEST extends LinearOpMode
+@TeleOp(name="Servo Control Test",group="Linear OpMode")
+public class Servo_Control_Test extends LinearOpMode
 {
     Servo armServoLeft;
     Servo armServoRight;
+
     @Override public void runOpMode() throws InterruptedException
     {
         armServoLeft = hardwareMap.servo.get("armServoLeft");
         armServoRight = hardwareMap.servo.get("armServoRight");
 
-        armServoLeft.setPosition(0.5144);
-        armServoRight.setPosition(0.39);
-
         GamepadValueMonitor valueMonitor = new GamepadValueMonitor(gamepad1, new GamepadValueMonitor.Fetchable<Boolean>() { @Override public Boolean fetch() { return opModeIsActive(); } })
         {{
-            dPadUp.updateInformer = new Runnable() { @Override public void run() { /* Code goes here. */ } };
+            dPadUp.updateInformer = new Runnable() { @Override public void run() { armServoLeft.setPosition(Servo.MAX_POSITION); armServoRight.setPosition(Servo.MIN_POSITION); } };
             dPadUp.active = true;
+
+            dPadDown.updateInformer = new Runnable() { @Override public void run() { armServoLeft.setPosition(Servo.MIN_POSITION); armServoRight.setPosition(Servo.MAX_POSITION); } };
+            dPadDown.active = true;
+
+            start.updateInformer = new Runnable() { @Override public void run() { armServoLeft.setPosition(0.5144); armServoRight.setPosition(0.39); } };
+            start.active = true;
         }};
+
         waitForStart();
+
+        valueMonitor.start.updateInformer.run();
         valueMonitor.startMonitoring(true);
     }
 }
